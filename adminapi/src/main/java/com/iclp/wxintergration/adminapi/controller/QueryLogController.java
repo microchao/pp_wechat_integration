e7d3c521a19be0d9f070fabb9d03d4517cc73d22
Jason.Shen <microchao225@gmail.com>
2018-08-30 16:41:06 +0800

    adminapi 接口

--- adminapi/src/main/java/com/iclp/wxintergration/adminapi/controller/QueryLogController.java
+++ adminapi/src/main/java/com/iclp/wxintergration/adminapi/controller/QueryLogController.java
@@ -1,14 +1,12 @@
 package com.iclp.wxintergration.adminapi.controller;
 
+import com.google.gson.Gson;
 import com.iclp.wxintergration.adminapi.springdata.repository.QueryLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Scope("request")
@@ -18,9 +16,8 @@ public class QueryLogController {
     private QueryLogRepository queryLogRepository;
 
     @RequestMapping(value="/queryLog",method =  { RequestMethod.GET, RequestMethod.POST })
-    public String queryLog(@RequestBody String jsonString,
-                           HttpServletRequest request) {
-        queryLogRepository.findAll();
-        return null;
+    public String queryLog() {
+        Gson gson = new Gson();
+        return gson.toJson(queryLogRepository.findAll());
     }
 }
--- adminapi/src/main/java/com/iclp/wxintergration/adminapi/springdata/repository/QueryLogRepository.java
+++ adminapi/src/main/java/com/iclp/wxintergration/adminapi/springdata/repository/QueryLogRepository.java
@@ -1,13 +1,10 @@
 package com.iclp.wxintergration.adminapi.springdata.repository;
 
+import com.iclp.wxintergration.adminapi.model.SearchLogModel;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-
-public interface QueryLogRepository extends CrudRepository {
+public interface QueryLogRepository extends CrudRepository<SearchLogModel,Integer> {
 
 
 }
+
