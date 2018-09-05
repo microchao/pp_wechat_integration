10ea5e16b262d4bb5e4a2ec29dec340659657604
Jason.Shen <microchao225@gmail.com>
2018-09-05 15:11:12 +0800

    xsocialapi

--- wxgateway/src/main/java/com/iclp/wechat/integration/wxgateway/controller/WxGatewayController.java
+++ wxgateway/src/main/java/com/iclp/wechat/integration/wxgateway/controller/WxGatewayController.java
@@ -1,6 +1,10 @@
 package com.iclp.wechat.integration.wxgateway.controller;
 
 import com.google.gson.Gson;
+import com.iclp.wechat.integration.wxgateway.model.XSocialModel;
+import org.json.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,23 +45,16 @@ public class WxGatewayController {
         }
         Map<String,String> map = new HashMap<>();
         map.put("name", name);
-        map.put("signature",signature);
+//        map.put("signature",signature);
         map.put("timestamp",timestamp);
-        map.put("nonce",nonce);
+//        map.put("nonce",nonce);
         map.put("openid",openid);
         map.put("echostr",echostr);
         Map requestMap = XmlUtil.parseXml(request);
+        String msgType = requestMap.get("MsgType").toString();
         logger.info("此次请求内容：" + requestMap);
-        if(requestMap.get("MsgType").equals("text")) {
-            map.put("content",requestMap.get("Content").toString());
-            map.put("toUserName",requestMap.get("ToUserName").toString());
-            Gson gson = new Gson();
-            String requestJson = gson.toJson(map);
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<String> requestParam = new HttpEntity<String>(requestJson, headers);
-            String object = restTemplate().postForObject("http://ppsearch:8888/txtsearch",requestParam,String.class);
-            return object;
+        if(msgType.equals("text")) {
+           return requestToMsgSearch(map,requestMap);
         }else{
             String xml = "<xml> " +
                     "       <ToUserName>"  + openid + "</ToUserName> " +
@@ -66,7 +65,49 @@ public class WxGatewayController {
                     "     </xml>";
             return xml;
         }
+    }
 
+    /**
+     *
+     * @return
+     */
+    @RequestMapping(value = "/xSocialSearch", method = {RequestMethod.GET, RequestMethod.POST})
+    public String xSocialSearch(HttpServletRequest request) {
+        Gson gson = new Gson();
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        XSocialModel xSocialModel = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+            xSocialModel = gson.fromJson(jb.toString(),XSocialModel.class);
+
+            System.out.println();
+        } catch (Exception e) { /*report an error*/ }
+        Map map = new HashMap();
+        map.put("toUserName",xSocialModel.getWeChatID());
+        map.put("openid",xSocialModel.getOpenid());
+//        map.put("nickname",nickname);
+        map.put("content",xSocialModel.getKeywords());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestJson = gson.toJson(map);
+        HttpEntity<String> requestParam = new HttpEntity<String>(requestJson, headers);
+        String object = restTemplate().postForObject("http://ppsearch:8888/txtsearch",requestParam,String.class);
+        return object;
+    }
+
+    public String requestToMsgSearch(Map map,Map requestMap) {
+        map.put("content",requestMap.get("Content").toString());
+        map.put("toUserName",requestMap.get("ToUserName").toString());
+        Gson gson = new Gson();
+        String requestJson = gson.toJson(map);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> requestParam = new HttpEntity<String>(requestJson, headers);
+        String object = restTemplate().postForObject("http://ppsearch:8888/txtsearch",requestParam,String.class);
+        return object;
     }
 
 
---
+++ wxgateway/src/main/java/com/iclp/wechat/integration/wxgateway/model/XSocialModel.java
@@ -0,0 +1,44 @@
+package com.iclp.wechat.integration.wxgateway.model;
+
+public class XSocialModel {
+
+    private String WeChatID;
+
+    private String Openid;
+
+    private String Nickname;
+
+    private String Keywords;
+
+    public String getWeChatID() {
+        return WeChatID;
+    }
+
+    public void setWeChatID(String weChatID) {
+        WeChatID = weChatID;
+    }
+
+    public String getOpenid() {
+        return Openid;
+    }
+
+    public void setOpenid(String openid) {
+        Openid = openid;
+    }
+
+    public String getNickname() {
+        return Nickname;
+    }
+
+    public void setNickname(String nickname) {
+        Nickname = nickname;
+    }
+
+    public String getKeywords() {
+        return Keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        Keywords = keywords;
+    }
+}
